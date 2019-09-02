9123772002d57588fa4187e3df6376340649b814
Christopher Proß <26790901+christopherpross@users.noreply.github.com>
2019-09-02 05:56:55 +0200

    fix many button names

--- addon/appmodules/lpwinmetro.py
+++ addon/appmodules/lpwinmetro.py
@@ -6,9 +6,9 @@ import controlTypes
 
 class AppModule(appModuleHandler.AppModule):
     def event_NVDAObject_init(self, obj):
-        if (obj.role == controlTypes.ROLE_RADIOBUTTON):
+        if (obj.role == controlTypes.ROLE_RADIOBUTTON) or (obj.role == controlTypes.ROLE_BUTTON):
 
-            # the object is a radiobutton
+            # the object is a radiobutton or a button, let's extract the children labels
             if (obj.childCount != 0):
                 nameresult = ""
                 for child in obj.children:
