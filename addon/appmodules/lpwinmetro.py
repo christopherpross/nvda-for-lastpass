65874b745c22f01d3ec6de47e4d8a6492fcaa70f
Christopher Proß <26790901+christopherpross@users.noreply.github.com>
2019-12-31 00:50:28 +0100

    - add a label for the "add"-button

--- addon/appmodules/lpwinmetro.py
+++ addon/appmodules/lpwinmetro.py
@@ -1,13 +1,13 @@
 import appModuleHandler
 import NVDAObjects
 import controlTypes
+import NVDAObjects.UIA
 
 
 
 class AppModule(appModuleHandler.AppModule):
-    def event_NVDAObject_init(self, obj):
+    def event_NVDAObject_init(self, obj : NVDAObjects.NVDAObject):
         if (obj.role == controlTypes.ROLE_RADIOBUTTON) or (obj.role == controlTypes.ROLE_BUTTON):
-
             # the object is a radiobutton or a button, let's extract the children labels
             if (obj.childCount != 0):
                 nameresult = ""
@@ -15,4 +15,12 @@ class AppModule(appModuleHandler.AppModule):
                     if (child.role == controlTypes.ROLE_STATICTEXT):
                         nameresult = child.name
                 obj.name = nameresult
+        if isinstance(obj, NVDAObjects.UIA.UIA):
+            # well, try to give the add button a label
+            obj.__class__ = NVDAObjects.UIA.UIA
+            if (obj.UIAElement.CurrentAutomationId =="sideAddButton"):
+                # the add button has this automationId at the moment
+                obj.name = "add"
+
+
 
